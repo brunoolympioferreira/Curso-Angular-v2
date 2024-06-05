c993384b19fedd5e67deba39b4c8301bbf5cc3db
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-06-05 15:51:41 -0300

    S3 - Utilizacao do seletor ::host

--- Secao_3_Componentes_E_Estilizacoes/projeto-components/src/app/app.module.ts
+++ Secao_3_Componentes_E_Estilizacoes/projeto-components/src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsModule } from './cards/cards.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CardsModule
   ],
   providers: [],
