9f54fb391e727abed4026401290c7467c2aa7734
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-06-18 10:47:03 -0300

    S4 - Attribute Binding

--- Secao4_Binding-Diretivas-Templates-Decorators/projeto2-secao4/src/app/app.component.ts
+++ Secao4_Binding-Diretivas-Templates-Decorators/projeto2-secao4/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto2-secao4';
+  buttonTitle = 'Título do botão'
+  buttonDisabled = false;
+
+  onButtonClick() {
+    this.buttonTitle = 'Titulo ALTERADOOOO';
+    this.buttonDisabled = !this.buttonDisabled;
+  }
 }
