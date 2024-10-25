dce0e1ab5ee093e769e8c6736aac8f0117d79119
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-10-25 11:23:27 -0300

    Data Pipe parte 1

--- fundamento-pipes/src/app/app.component.ts
+++ fundamento-pipes/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export enum UserStatusEnum {
   ATIVO = 1,
@@ -10,10 +10,21 @@ export enum UserStatusEnum {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   user = {
     nome: 'Bruno',
     idade: 38,
     status: UserStatusEnum.ATIVO
   };
+
+  minhaData: string = '2024-10-25T21:00:00.000Z';
+
+  minhaDataObj: Date = new Date('2024-10-24T21:00:00.000Z');
+
+  ngOnInit() {
+    console.log('Data convertida para região:', new Date(this.minhaData))
+    console.log('Data UTC-0:', new Date(this.minhaData).toUTCString())
+    console.log('Timestamp', new Date(this.minhaData).getTime());
+    console.log(this.minhaDataObj);
+  }
 }
