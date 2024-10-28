3089b185110cb99c9dfd1b5b53cdc264f5d810e4
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-10-28 14:53:31 -0300

    Pipe Data - Parte 3

--- fundamento-pipes/src/app/app.module.ts
+++ fundamento-pipes/src/app/app.module.ts
@@ -6,6 +6,19 @@ import { AppComponent } from './app.component';
 import { UserStatusPipe } from './pipes/user-status.pipe';
 import { TruncatePipe } from './pipes/truncate.pipe';
 
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+
+import { DATE_PIPE_DEFAULT_OPTIONS, DatePipeConfig } from '@angular/common';
+
+const datePipeConfig: DatePipeConfig = {
+  dateFormat: 'dd/MM/YYYY',
+  timezone: '+0000'
+};
+
+registerLocaleData(localePt, 'pt-BR');
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +29,16 @@ import { TruncatePipe } from './pipes/truncate.pipe';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: datePipeConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
