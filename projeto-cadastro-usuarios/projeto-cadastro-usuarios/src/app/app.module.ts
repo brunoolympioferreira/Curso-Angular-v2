c36ccd2592c045e844ad19bd5412b53b4062593c
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-12-18 17:28:42 -0300

    Criacao dos Modulos

--- projeto-cadastro-usuarios/projeto-cadastro-usuarios/src/app/app.module.ts
+++ projeto-cadastro-usuarios/projeto-cadastro-usuarios/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ComponentsModule } from './components/components.module';
 
+import { AppComponent } from './app.component';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    HttpClientModule
   ],
   providers: [
     provideAnimationsAsync()
