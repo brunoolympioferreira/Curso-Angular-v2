d5f5959cb11c7e19f8bd1726484d82a619e562ca
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-12-20 12:35:25 -0300

    Criacao das Services

--- projeto-cadastro-usuarios/projeto-cadastro-usuarios/src/app/app.component.ts
+++ projeto-cadastro-usuarios/projeto-cadastro-usuarios/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
+import { GenresService } from './services/genres.service';
+import { BrazilianStatesService } from './services/brazilian-states.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'projeto-cadastro-usuarios';
+export class AppComponent implements OnInit {
+  usersList: any = [];
+  genresList: any = [];
+  statesList: any = [];
+
+  constructor(
+    private readonly _usersService: UsersService,
+    private readonly _genresService: GenresService,
+    private readonly _brazilianStatesService: BrazilianStatesService,
+  ) { }
+
+  ngOnInit() {
+    this.getUsers();
+    this.getGenres();
+    this.getStates();
+  }
+
+  private getStates() {
+    this._brazilianStatesService.getStates().subscribe((statesListResponse) => {
+      this.statesList = statesListResponse;
+    })
+  }
+
+  private getGenres() {
+    this._genresService.getGenres().subscribe((genresListResponse) => {
+      this.genresList = genresListResponse
+    });
+  }
+
+  private getUsers() {
+    this._usersService.getUsers().subscribe((usersListResponse) => {
+      this.usersList = usersListResponse;
+    });
+  }
 }
