e774b03878a2b4e2e233d2edab359bc5c3c2b7b6
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-12-27 16:30:58 -0300

    Criacao do componente UsersCardListComponent

--- projeto-cadastro-usuarios/projeto-cadastro-usuarios/src/app/components/components.module.ts
+++ projeto-cadastro-usuarios/projeto-cadastro-usuarios/src/app/components/components.module.ts
@@ -5,16 +5,21 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AngularMaterialModule } from "../angular-material.module";
 import { DirectivesModule } from "../directives/directives.module";
 import { PipesModule } from "../pipes/pipes.module";
+import { UsersCardListComponent } from './users-card-list/users-card-list.component';
 
 @NgModule({
-    declarations: [],
-    imports: [
-        FormsModule,
-        BrowserModule,
-        AngularMaterialModule,
-        DirectivesModule,
-        PipesModule
-    ],
-    exports: [],
+  declarations: [
+    UsersCardListComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AngularMaterialModule,
+    DirectivesModule,
+    PipesModule
+  ],
+  exports: [
+    UsersCardListComponent,
+  ],
 })
 export class ComponentsModule { }
