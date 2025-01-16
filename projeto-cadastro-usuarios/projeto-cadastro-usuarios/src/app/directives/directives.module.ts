776f3c995f61ea154af5f380c41dcfbb0222a5ed
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-01-16 15:39:51 -0300

    Criacao do Campo de confirmacao de senha

--- projeto-cadastro-usuarios/projeto-cadastro-usuarios/src/app/directives/directives.module.ts
+++ projeto-cadastro-usuarios/projeto-cadastro-usuarios/src/app/directives/directives.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from "@angular/core";
 import { EmailPatternValidatorDirective } from './email-pattern-validator.directive';
 import { CredentialsValidatorDirective } from './credentials-validator.directive';
 import { PasswordStrenghtValidatorDirective } from './password-strenght-validator.directive';
+import { PasswordConfirmationValidatorDirective } from './password-confirmation-validator.directive';
 
 @NgModule({
   declarations: [
     EmailPatternValidatorDirective,
     CredentialsValidatorDirective,
-    PasswordStrenghtValidatorDirective
+    PasswordStrenghtValidatorDirective,
+    PasswordConfirmationValidatorDirective
   ],
   exports: [
     EmailPatternValidatorDirective,
     CredentialsValidatorDirective,
     PasswordStrenghtValidatorDirective,
+    PasswordConfirmationValidatorDirective,
   ],
 })
 export class DirectivesModule { }
